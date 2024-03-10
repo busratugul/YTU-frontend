0482964f15b603e70d4792f78fa702eb4553a937
Busra Tugul <bsratugul@gmail.com>
2024-03-10 19:46:55 +0300

    localstorega getItem çalışması

--- week9/src/App.js
+++ week9/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Kisi from './components/Kisi'
 
 function App() {
+  const [veri, setVeri] = useState([])
+
+  useEffect(() => {
+    async function rehberCek() {
+      const rehberJSON = await localStorage.getItem('rehberJSON')
+      const rehberVeri = JSON.parse(rehberJSON)
+
+      setVeri(rehberVeri)
+    }
+    rehberCek()
+  }, [])
+
   return (
-    <div>App</div>
+    <>
+      <section className="container mt-5">
+        <h1>Rehber</h1>
+        <div className="row">
+          {veri.map((eleman, idx) => (
+            <Kisi ad={eleman.ad} tel={eleman.tel} key={idx} />
+          ))}
+        </div>
+      </section>
+    </>
   )
 }
 
