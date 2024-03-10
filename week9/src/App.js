af18aa8ba68267dcbd01a171a03677a54a01723f
Busra Tugul <bsratugul@gmail.com>
2024-03-10 18:51:29 +0300

    use effect kullanımına örnek yapıldı

--- week9/src/App.js
+++ week9/src/App.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  //const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://jsonplaceholder.org/users')
+      const jsObjectData = await response.json()
+      setData(jsObjectData)
+      //setLoading(false)
+    }
+
+    setTimeout(fetchData, 2000)
+  }, [])
+  console.log(data)
   return (
-    <div>App</div>
+    <>
+      {/*  {loading && <h1>Loading...</h1> } */}
+      {data.length === 0 ? (
+        <h1>Loading...</h1>
+      ) : (
+        data.map((kisi, idx) => {
+          return (
+            <p key={idx}>
+              {' '}
+              {kisi.firstname} {kisi.lastname}{' '}
+            </p>
+          )
+        })
+      )}
+    </>
   )
 }
 
