7dff567da2319bf80efbeb68236c56b8dc3f9530
Busra Tugul <bsratugul@gmail.com>
2024-03-12 15:48:17 +0300

    odev19 tamamlandı

---
+++ Ödevler/odev19/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UsersData from './components/Users'
+
+function App() {
+  return (
+    <div className="container mt-5 border">
+      <h1 className="text-center my-3">SEARCH FILTER APP</h1>
+      <UsersData />
+    </div>
+  )
+}
+
+export default App
