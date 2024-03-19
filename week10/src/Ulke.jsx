cfe703fae8c9fdea838ab6323ffa4c95b60d3e2d
Busra Tugul <bsratugul@gmail.com>
2024-03-19 17:32:24 +0300

    region propertysine göre filtreleme özelliği getirildi.

--- week10/src/Ulke.jsx
+++ week10/src/Ulke.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-function Ulke({ulkeVeri}) {
+function Ulke({ ulkeVeri }) {
   return (
-    <section className='col-12 col-sm-6 col-md-4 col-lg-3'>
-       <div className='border rounded-3 p-3 border-dark border-3 d-flex bg-secondary shadow text-left' style={{justifyContent:"space-between"}}>
-<h6 className='fs-10'><strong>{ulkeVeri.name.official}</strong></h6>
-<img src={ulkeVeri.flags.png} width="30px" height="15px"/>
-       </div>
+    <section className="col-12 col-sm-6 col-md-4 col-lg-3">
+      <div
+        className="border rounded-3 p-3 border-dark border-3 d-flex bg-secondary shadow text-left"
+        style={{ justifyContent: 'space-between' }}
+      >
+        <h6 className="fs-10">
+          <strong>{ulkeVeri.name.official}</strong>
+        </h6>
+        <img src={ulkeVeri.flags.png} width="30px" height="15px" />
+      </div>
     </section>
   )
 }
--- week10/src/UlkeList.jsx
+++ week10/src/UlkeList.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react'
 import Ulke from './Ulke'
 
 function UlkeList() {
+  //STATES
   const [hata, setHata] = useState(null)
   const [yukleniyor, setYukleniyor] = useState(true)
   const [ulkeler, setUlkeler] = useState([])
+  //arama işlemleri
   const [arama, setArama] = useState('')
-  //const [aramaAlanları] = useState(['name', 'capital'])
+  const [filterParam, setFilterParam] = useState('Tümü')
 
   useEffect(() => {
     async function veriCek() {
@@ -15,7 +17,7 @@ function UlkeList() {
         const JSObjesi = await yanit.json()
         setUlkeler(JSObjesi)
         setYukleniyor(false)
-        console.log(ulkeler)
+        //console.log(ulkeler)
       } catch {
         setHata(false)
       }
@@ -28,16 +30,17 @@ function UlkeList() {
   }
 
   if (yukleniyor) {
-    return <main>Yukleniyor...</main>
+    return <main>Yükleniyor...</main>
   }
 
   function filtrele() {
     return ulkeler.filter((ulke) => {
+      if (filterParam !== 'Tümü') if (ulke.region !== filterParam) return
+
       const aranan = arama.toLowerCase()
+
+      //eğer herhangi bir kıta seçildiyse veya tümü seçildiyse
       if (ulke.name?.common.toLowerCase().includes(aranan)) {
-        return true
-      }
-      if (ulke.name?.nativeName.toLowerCase().includes(aranan)) {
         return true
       }
       if (ulke.name?.official.toLowerCase().includes(aranan)) {
@@ -47,7 +50,9 @@ function UlkeList() {
 
       if (
         ulke.name['nativeName'] &&
-        ulke.name?.nativeName[ulkeKisaltma].official.toLowerCase().includes(aranan)
+        ulke.name?.nativeName[ulkeKisaltma]?.official
+          .toLowerCase()
+          .includes(aranan)
       ) {
         return true
       }
@@ -67,28 +72,33 @@ function UlkeList() {
     })
   }
 
-  /*   function filtrele(ulkeler) {
-    return ulkeler.filter((item) => {
-      return ( 
-        aramaAlanları.some((newItem) => {
-            if(item[newItem]) {
-                return item[newItem].toString().toLowerCase().indexOf(arama.toLowerCase()) > 1 }
-            })
-      })
-    })
-} */
-
   return (
     <main className="container">
       <h2 className="text-danger my-3 text-center">
-        <strong>Ulke List</strong>
+        <strong>Country List</strong>
       </h2>
       <div>
         <input
+          className="form-control mb-3 border-3"
           type="search"
           value={arama}
           onChange={(e) => setArama(e.target.value)}
+          placeholder="Search..."
         />
+        <select
+          onChange={(e) => {
+            setFilterParam(e.target.value)
+          }}
+          className="form-select mb-3"
+          aria-label="Filter Countries By Region"
+        >
+          <option value="Tümü">Tümü</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
       </div>
       <div className="row g-3">
         {filtrele(ulkeler).map((ulke, idx) => (
